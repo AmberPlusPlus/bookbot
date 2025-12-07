0c33cb9df0d2e2426c2c14788379a4a4fd4cadab
AmberPlusPlus <amberschock25@gmail.com>
2025-12-06 21:11:47 -0500

    added character count with sorting

--- main.py
+++ main.py
@@ -1,13 +1,43 @@
-from stats import word_count
+from stats import word_count, char_count, dict_sort
 
 def get_book_text(filepath):
     with open(filepath) as f:
         file_contents = f.read()
     return file_contents
 
+def sort_on(items):
+    return items["num"]
+
 def main():
 
-    wordcount = word_count(get_book_text(("./books/frankenstein.txt")))
+    bookfile = "books/frankenstein.txt"
+    text = get_book_text(bookfile)
+
+    wordcount = word_count(text)
+    charcount = char_count(text)
+    # print(charcount)
+
+    dict_list = []
+    for entry in charcount:
+        if not entry[0].isalpha():
+            continue
+        #print(entry[0])
+        dict_list.append({"char": entry[0], "num": charcount[entry[0]]})
+
+    dict_sort(dict_list)
+
+    # print(dict_list)
+
+    # output
+
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {bookfile}...")
+    print("----------- Word Count ----------")
     print(f"Found {wordcount} total words")
-          
+    print("--------- Character Count -------")
+    for entry in dict_list:
+        #print(entry)
+        print(f"{entry['char']}: {entry['num']}")
+    print("============= END ===============")
+
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,23 @@
 def word_count(text):
     word_list = text.split()
     return len(word_list)
+
+def char_count(text):
+    text = text.lower()
+
+    chars = {}
+
+    for char in text:
+        if not char in chars:
+            chars[char] = 1
+        else:
+            chars[char] += 1
+    
+    return chars
+
+def sort_on(items):
+    return items['num']
+
+def dict_sort(dictionary):
+    dictionary.sort(reverse=True, key=sort_on)
+    # print(dictionary)
